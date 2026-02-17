97e8a489aebb3e07ca9e9e553c3a7c25a950901c
anishcletus <anishcletus@gmail.com>
2026-02-17 08:36:38 -0800

    Add files via upload

---
+++ database.js
@@ -0,0 +1,203 @@
+const externalMovies = [
+  {
+    "id": 1770919776473,
+    "title": "Baby Girl (Film)",
+    "year": "2026",
+    "genre": "Drama Thriller",
+    "url": "https://youtu.be/05_wGR3dRRo",
+    "ottUrl": "https://www.sonyliv.com/movies/baby-girl-1590014705?utm_source=Share&utm_medium=social_share",
+    "director": "Arun Varma",
+    "lang": "Malayalam",
+    "cast": [
+      "Nivin Pauly Sangeeth Prathap Lijomol Jose Abhimanyu Shammi Thilakan"
+    ],
+    "poster": "https://upload.wikimedia.org/wikipedia/en/b/b9/Baby_Girl_poster.jpg",
+    "tags": [
+      "Drama",
+      "Social"
+    ],
+    "ratings": [
+      {
+        "scores": {
+          "Story": 2.5,
+          "Character / Performance": 2.5,
+          "Direction / Vision": 2.5,
+          "Cinematography / Visuals": 2.5,
+          "Editing / Pacing": 2.5,
+          "Sound / Music": 1.5,
+          "Thematic / Emotional Cohesion": 2
+        }
+      }
+    ],
+    "review": "Occasionally Engaging - Baby Girl serves primarily as a showcase for Sangeeth Prathap. Beyond highlighting his talent, there is little else of note, though it stands as a clear launchpad for his potential to be fully realised in stronger material.",
+    "experience": {
+      "Emotion": 1.5,
+      "Attention": 2.5,
+      "Connection": 2.5,
+      "Impact": 2,
+      "Engagement": 2
+    },
+    "synopsis": "Baby Girl follows a hospital attendant played by Nivin Pauly, whose life intertwines with that of Rishi (Sangeeth Prathap’s character) and his sister when a newborn goes missing, forcing them into a tense search that exposes fear, guilt and the emotional cost of truth and responsibility."
+  },
+  {
+    "id": 1771040226122,
+    "title": "Theatre: The Myth Of Reality",
+    "year": "2025",
+    "genre": "Mystery",
+    "url": "https://youtu.be/6KLkZ6wNiLE?si=WQP5CX-1HSSfYfSp",
+    "ottUrl": "",
+    "director": "Sajin Baabu",
+    "lang": "Malayalam",
+    "cast": [
+      "Rima Kallingal Sarasa Balussery"
+    ],
+    "poster": "https://upload.wikimedia.org/wikipedia/en/f/f2/Theatre_2025.jpg",
+    "tags": [
+      "Mystic"
+    ],
+    "ratings": [
+      {
+        "scores": {
+          "Story": 3,
+          "Character / Performance": 3.5,
+          "Direction / Vision": 3.5,
+          "Cinematography / Visuals": 3.5,
+          "Editing / Pacing": 3,
+          "Sound / Music": 3,
+          "Thematic / Emotional Cohesion": 2.5
+        }
+      }
+    ],
+    "review": "Watchworthy, Stirring - Through a measured lens, the film dissects society and reality, illustrating how social media and traditional customs shape belief and spectacle in a world where myth and truth collide",
+    "experience": {
+      "Emotion": 3.5,
+      "Attention": 3,
+      "Connection": 3,
+      "Impact": 2.5,
+      "Engagement": 2
+    },
+    "synopsis": "A mother and daughter living in a secluded Kerala island steeped in ancient tradition are forced to confront superstition, belief and modern spectacle when misfortune disrupts their isolated life, revealing how myth, social media and cultural perception shape truth and reality."
+  },
+  {
+    "id": 1771043929781,
+    "title": "How To Get To Heaven From Belfast",
+    "year": "2026",
+    "genre": "Dark Comedy Crime Thriller",
+    "url": "",
+    "ottUrl": "https://www.netflix.com/ae-en/title/81758849",
+    "director": "Lisa Mcgee",
+    "lang": "English",
+    "cast": [
+      "Roisin Gallagher SinéAd Keenan Caoilfhionn Dunne"
+    ],
+    "poster": "",
+    "tags": [
+      "Netflix",
+      "Series",
+      "Quirky",
+      "Mystery",
+      "Friendship"
+    ],
+    "ratings": [
+      {
+        "scores": {
+          "Story": 4,
+          "Character / Performance": 4.5,
+          "Direction / Vision": 4,
+          "Cinematography / Visuals": 3.5,
+          "Editing / Pacing": 3.5,
+          "Sound / Music": 4,
+          "Thematic / Emotional Cohesion": 3.5
+        }
+      }
+    ],
+    "review": "Absorbing, Engaging - Blending suspense, surreal humor, and 2000s nostalgia, the series thrives on the chemistry of its central trio and their authentic, emotionally resonant friendships. With unpredictable twists, kitsch adventures, and a “bonkers but brilliant” tone, it’s a quirky, character driven ride that balances comedy, drama, and mystery — a must watch for fans of Derry Girls (Lisa McGee) and anyone who enjoys darkly comic, chaotic storytelling.",
+    "experience": {
+      "Emotion": 3,
+      "Attention": 3.5,
+      "Connection": 3,
+      "Impact": 2.5,
+      "Engagement": 4.5
+    },
+    "synopsis": "Follows three friends drawn back into old secrets after a mysterious death, sending them on a darkly comic journey across Ireland where friendship, guilt, and chaos collide."
+  },
+  {
+    "id": 1771175637082,
+    "title": "Thalaivar Thambi Thalaimaiyil",
+    "year": "2026",
+    "genre": "Political Satire",
+    "url": "https://youtu.be/qFR33nk-7Qc?si=xcA4m2iTyhP05L4X",
+    "ottUrl": "https://www.netflix.com/ae-en/title/82709712",
+    "director": "Nithish Sahadev",
+    "lang": "Tamil",
+    "cast": [
+      "Jiiva Thambi Ramaiah Ilavarasu Prathana Nathan"
+    ],
+    "poster": "https://upload.wikimedia.org/wikipedia/en/5/56/Thalaivar_Thambi_Thalaimaiyil.jpg",
+    "synopsis": "TTT centres on village panchayat head Jeevarathnam, who must keep peace when a wedding and a funeral clash in the same village morning, turning a simple celebration into a chaotic comedy of ego, tradition and community conflict.",
+    "tags": [
+      "Netflix",
+      "Lighthearted",
+      "Fun",
+      "Chaotic"
+    ],
+    "ratings": [
+      {
+        "scores": {
+          "Story": 2.5,
+          "Character / Performance": 2.5,
+          "Direction / Vision": 2.5,
+          "Cinematography / Visuals": 2.5,
+          "Editing / Pacing": 2.5,
+          "Sound / Music": 2.5,
+          "Thematic / Emotional Cohesion": 2
+        }
+      }
+    ],
+    "review": "Occasionally Engaging - TTT marks the Tamil collaboration of Nitish Sahadev following his Malayalam debut success Falimy. The movie carries itself well in the first half, slows down a bit later, but manages a decent ending. It may not fully hold your attention throughout or linger in memory, yet it’s a fun, light watch with a few noticeable mistakes.",
+    "experience": {
+      "Emotion": 2,
+      "Attention": 2.5,
+      "Connection": 2.5,
+      "Impact": 2,
+      "Engagement": 3
+    }
+  },
+  {
+    "id": 1771345212942,
+    "title": "Aashaan",
+    "year": "2026",
+    "genre": "Comedy Drama",
+    "url": "https://youtu.be/Jq9bD1EWge8?si=Z2fPGs52v-xsYGBT",
+    "ottUrl": "",
+    "director": "Johnpaul George",
+    "lang": "Malayalam",
+    "cast": [
+      "Indrans Joemon Jyothir Shobi Thilakan Madan Gowri Bibin Perumpally Abin Bino Kudassanad Kanakam"
+    ],
+    "poster": "https://upload.wikimedia.org/wikipedia/en/7/7b/Aashaan.jpg",
+    "synopsis": "Aashaan is a comedy drama about an elderly film enthusiast and a young VFX hopeful, chasing their filmmaking dream, only to find themselves caught between ambition, ego, and the unpredictable realities of the movie world.",
+    "tags": [],
+    "ratings": [
+      {
+        "scores": {
+          "Story": 2,
+          "Character / Performance": 2.5,
+          "Direction / Vision": 2.5,
+          "Cinematography / Visuals": 2.5,
+          "Editing / Pacing": 2.5,
+          "Sound / Music": 3,
+          "Thematic / Emotional Cohesion": 2
+        }
+      }
+    ],
+    "review": "Occasionally Engaging - Aashaan attempts to capture the passion, struggles, ego clashes, and generational differences within the filmmaking world. It paints an interesting picture of how ambition collides with experience, and how cinema means different things to different generations. However, the script struggles to stay grounded, trying to explore too many ideas without fully committing to any one of them. The “film within a film” concept, though promising, feels underdeveloped. It leans heavily into the technical process of filmmaking rather than focusing on crafting a compelling inner story, which ultimately weakens the emotional payoff. Despite sincere performances from the cast, their efforts are let down by inconsistent writing and structural flaws. While Aashaan might work as a casual, light watch on television, it doesn’t quite deliver the depth or engagement expected for a theatrical experience.",
+    "experience": {
+      "Emotion": 2,
+      "Attention": 2,
+      "Connection": 2,
+      "Impact": 2,
+      "Engagement": 2
+    }
+  }
+];
